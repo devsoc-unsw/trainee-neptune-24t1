0aa00a10cd5183fc01772ca5fc2857741c97213d
Peter Yang <ypete@Peters-Laptop.local>
2024-05-19 21:40:03 +1000

    added hover transitions to nav

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 function NavigationBar () {
   return (
     <>
-      <nav className="bg-cyan-500 m-0 min-w-full h-12 flex justify-evenly items-center font-raleway text-white">
+      <nav className="bg-cyan-500 transition duration-500 opacity-75 hover:opacity-100 m-0 min-w-full h-16 flex justify-evenly items-center font-raleway text-white">
         <div>
           <Link to="/">
             <img src="logo.png" alt="logo" className="max-w-8 max-h-8"/>
           </Link>
         </div>
         <div>
-          <Link to="/guides">PUZZLE GUIDES</Link>
+          <Link to="/guides" className="transition duration-700 bg-inherit hover:bg-cyan-600 p-3">PUZZLE GUIDES</Link>
         </div>
         <div>
-          <Link to="/archive">HUNT ARCHIVE</Link>
+          <Link to="/archive" className="transition duration-700 bg-inherit hover:bg-cyan-600 p-3">HUNT ARCHIVE</Link>
         </div>
         <div>
-          <Link to="/comp">WEEKLY COMP</Link>
+          <Link to="/comp" className="transition duration-700 bg-inherit hover:bg-cyan-600 p-3">WEEKLY COMP</Link>
         </div>
       </nav>
     </>
