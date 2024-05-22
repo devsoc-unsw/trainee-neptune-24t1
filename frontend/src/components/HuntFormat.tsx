5c5126d24f0467794333badb69a6bf8a002db049
Jian Wang <z5482749@ad.unsw.edu.au>
2024-05-22 17:27:57 +1000

    Changed nav z-index to 10 and archive box margins/paddings

--- frontend/src/components/HuntFormat.tsx
+++ frontend/src/components/HuntFormat.tsx
@@ -17,16 +17,16 @@ const HuntFormat: React.FC<HuntDetails> = ({img, title, pdf, answer, solution}) 
     solution = "Unavailable.";
   }
   return (
-    <div className="max-w-96 my-5 mx-5 bg-gray-100 px-5 py-8 rounded-lg">
+    <div className="max-w-96 my-5 mx-5 bg-slate-100 py-3 px-3 rounded-md">
       <div className="flex flex-col items-start">
-        <img src={img} className="aspect-video"></img>
+        <img src={img} className="aspect-video rounded"></img>
         <h1 className="place-self-center font-raleway rounded my-5 text-2xl"><b> {title}</b></h1>
-        <p>PDF:</p>
-        {pdf !== "Unavailable." ? (<a href={pdf} className="text-blue-500 underline hover:text-blue-900">{pdf}</a>) : <span><i>{pdf}</i></span>}
-        <p>Answer Checking Form:</p>
-        {answer !== "Unavailable." ? (<a href={answer} className="text-blue-500 underline hover:text-blue-900">{answer}</a>) : <span><i>{answer}</i></span>}
-        <p>Solutions:</p>
-        {solution !== "Unavailable." ? (<a href={solution} className="text-blue-500 underline hover:text-blue-900">{solution}</a>) : <span><i>{solution}</i></span>}
+        <p className="px-2">PDF:</p>
+        {pdf !== "Unavailable." ? (<a href={pdf} className="px-2 text-blue-500 underline hover:text-blue-900">{pdf}</a>) : <span><i>{pdf}</i></span>}
+        <p className="px-2">Answer Checking Form:</p>
+        {answer !== "Unavailable." ? (<a href={answer} className="px-2 text-blue-500 underline hover:text-blue-900">{answer}</a>) : <span><i>{answer}</i></span>}
+        <p className="px-2">Solutions:</p>
+        {solution !== "Unavailable." ? (<a href={solution} className="px-2 text-blue-500 underline hover:text-blue-900">{solution}</a>) : <span><i>{solution}</i></span>}
       </div>
     </div>
   );
