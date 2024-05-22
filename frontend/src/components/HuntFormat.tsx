993b156a7742dfc085ff5fd42131cc6fc82cd0fb
Jian Wang <z5482749@ad.unsw.edu.au>
2024-05-22 18:20:31 +1000

    Fixed padding for unavailable display message

--- frontend/src/components/HuntFormat.tsx
+++ frontend/src/components/HuntFormat.tsx
@@ -22,11 +22,11 @@ const HuntFormat: React.FC<HuntDetails> = ({img, title, pdf, answer, solution}) 
         <img src={img} className="aspect-video rounded"></img>
         <h1 className="place-self-center font-raleway rounded my-5 text-2xl"><b> {title}</b></h1>
         <p className="px-2">PDF:</p>
-        {pdf !== "Unavailable." ? (<a href={pdf} className="px-2 text-blue-500 underline hover:text-blue-900">{pdf}</a>) : <span><i>{pdf}</i></span>}
+        {pdf !== "Unavailable." ? (<a href={pdf} className="px-2 text-blue-500 underline hover:text-blue-900">{pdf}</a>) : <span className="px-2"><i>{pdf}</i></span>}
         <p className="px-2">Answer Checking Form:</p>
-        {answer !== "Unavailable." ? (<a href={answer} className="px-2 text-blue-500 underline hover:text-blue-900">{answer}</a>) : <span><i>{answer}</i></span>}
+        {answer !== "Unavailable." ? (<a href={answer} className="px-2 text-blue-500 underline hover:text-blue-900">{answer}</a>) : <span className="px-2"><i>{answer}</i></span>}
         <p className="px-2">Solutions:</p>
-        {solution !== "Unavailable." ? (<a href={solution} className="px-2 text-blue-500 underline hover:text-blue-900">{solution}</a>) : <span><i>{solution}</i></span>}
+        {solution !== "Unavailable." ? (<a href={solution} className="px-2 text-blue-500 underline hover:text-blue-900">{solution}</a>) : <span className="px-2"><i>{solution}</i></span>}
       </div>
     </div>
   );
--- frontend/src/components/OWeekFormat.tsx
+++ frontend/src/components/OWeekFormat.tsx
@@ -18,9 +18,9 @@ const OWeekFormat: React.FC<OWeekDetails> = ({img, title, form, solution}) => {
         <img src={img} className="aspect-video rounded"></img>
         <h1 className="px-2 place-self-center font-raleway rounded my-5 text-2xl"><b> {title}</b></h1>
         <p className="px-2">Google Forms:</p>
-        {form !== "Unavailable." ? (<a href={form} className="px-2 text-blue-500 underline hover:text-blue-900">{form}</a>) : <span><i>{form}</i></span>}
+        {form !== "Unavailable." ? (<a href={form} className="px-2 text-blue-500 underline hover:text-blue-900">{form}</a>) : <span className="px-2"><i>{form}</i></span>}
         <p className="px-2">Solutions:</p>
-        {solution !== "Unavailable." ? (<a href={solution} className="px-2 text-blue-500 underline hover:text-blue-900">{solution}</a>) : <span><i>{solution}</i></span>}
+        {solution !== "Unavailable." ? (<a href={solution} className="px-2 text-blue-500 underline hover:text-blue-900">{solution}</a>) : <span className="px-2"><i>{solution}</i></span>}
       </div>
     </div>
   );
