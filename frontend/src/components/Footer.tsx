3f47c86cc4c110ac79923d335c0028ae36cb65d1
Peter Yang <ypete@Peters-Laptop.local>
2024-05-24 00:24:42 +1000

    changed social media links to open in new tab

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,19 +9,19 @@ function Footer () {
       <div className="bg-slate-100 flex justify-between p-6 fixed bottom-0 min-w-full">
           <span>&copy; UNSW Puzzle Society</span>
           <div className="flex space-x-6">
-            <a href="https://discord.gg/2MqMrs2MG8" className="text-[#7289da] hover:text-blue-900 transition duration-300">
+            <a href="https://discord.gg/2MqMrs2MG8" target="_blank" className="text-[#7289da] hover:text-blue-900 transition duration-300">
               <FontAwesomeIcon icon={faDiscord} size="lg" />
             </a>
-            <a href="https://www.instagram.com/puzzlesoc_unsw/" className="text-pink-600 hover:text-pink-900 transition duration-300">
+            <a href="https://www.instagram.com/puzzlesoc_unsw/" target="_blank" className="text-pink-600 hover:text-pink-900 transition duration-300">
               <FontAwesomeIcon icon={faInstagram} size="lg" />
             </a>
-            <a href="https://www.facebook.com/UNSWPuzzleSoc" className="text-blue-600 hover:text-blue-900 transition duration-300">
+            <a href="https://www.facebook.com/UNSWPuzzleSoc" target="_blank" className="text-blue-600 hover:text-blue-900 transition duration-300">
               <FontAwesomeIcon icon={faFacebook} size="lg" />
             </a>
-            <a href="https://www.youtube.com/@unswpuzzlesoc/" className="text-red-600 hover:text-red-900 transition duration-300">
+            <a href="https://www.youtube.com/@unswpuzzlesoc/" target="_blank" className="text-red-600 hover:text-red-900 transition duration-300">
               <FontAwesomeIcon icon={faYoutube} size="lg" />
             </a>
-            <a href="mailto:unswpuzzlesoc@gmail.com" className="text-slate-600 hover:text-slate-900 transition duration-300">
+            <a href="mailto:unswpuzzlesoc@gmail.com" className="text-slate-400 hover:text-slate-800 transition duration-300">
               <FontAwesomeIcon icon={faEnvelope} size="lg" />
             </a>
           </div>
