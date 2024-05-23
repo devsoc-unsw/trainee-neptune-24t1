bf05cf0821bcb2a60a319e6e8b07a1bc49f7c559
Peter Yang <ypete@Peters-Laptop.local>
2024-05-24 00:21:17 +1000

    styled lists

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -4,20 +4,29 @@ import NavigationBar from '../components/Navigation';
 import Footer from '../components/Footer';
 import SectionHeader from '../components/SectionHeading';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons/faPuzzlePiece';
+
 function Landing () {
   return (
     <>
       <NavigationBar/>
-      <SectionHeader heading="ABOUT US"/>
-      <p className="p-2 font-raleway">
-        We are a hobby society for individuals with a passion for problem-solving!
-        <li>Learn rebus, logic, and cryptic puzzles</li>
-        <li>Compete and win in puzzle hunts</li>
-        <li>Join a fun, social, and interactive community</li>
-        <li>Develop creative and problem-solving skills</li>
-      </p>
-      <SectionHeader heading="MEET THE TEAM"/>
-      <SectionHeader heading="OUR SPONSORS"/>
+
+      <div className="flex flex-col items-center">
+        <SectionHeader heading="ABOUT US"/>
+        <p className="flex flex-col items-center p-2 font-raleway text-left">
+          We are a hobby society for individuals with a passion for problem-solving!
+          <ul>
+            <li><FontAwesomeIcon icon={faPuzzlePiece} /> Learn rebus, logic, and cryptic puzzles</li>
+            <li><FontAwesomeIcon icon={faPuzzlePiece} /> Compete and win in puzzle hunts</li>
+            <li><FontAwesomeIcon icon={faPuzzlePiece} /> Join a fun, social, and interactive community</li>
+            <li><FontAwesomeIcon icon={faPuzzlePiece} /> Develop creative and problem-solving skills</li>
+          </ul>
+        </p>
+        <SectionHeader heading="MEET THE TEAM"/>
+        <SectionHeader heading="OUR SPONSORS"/>
+      </div>
+
       <Footer/>
     </>
   );
