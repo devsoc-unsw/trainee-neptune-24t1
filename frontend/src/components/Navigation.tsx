47e0c0593f1028e1d605aea719509f45c314bf4d
Peter Yang <z5476245@ad.unsw.edu.au>
2024-05-31 02:14:00 +1000

    even more responsive

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -27,20 +27,20 @@ function NavigationBar () {
       <nav className="z-20 sticky top-0 bg-puzzleBlue transition duration-200 opacity-90 hover:opacity-100 m-0 min-w-full h-14 px-2 flex justify-evenly items-center font-raleway text-white">
         <div>
           <Link to="/">
-            <img src="logo.png" alt="logo" className="w-10 h-10 sm:w-12 sm:h-12 transition duration-200 contrast-125 brightness-100 hover:brightness-95"/>
+            <img src="logo.png" alt="logo" className="w-8 h-8 sm:w-12 sm:h-12 transition duration-200 contrast-125 brightness-100 hover:brightness-95"/>
           </Link>
         </div>
         <div>
-          <Link to="/events" className="transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">UPCOMING&nbsp;</span>EVENTS</Link>
+          <Link to="/events" className="text-xs sm:text-base transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">UPCOMING&nbsp;</span>EVENTS</Link>
         </div>
         <div>
-          <Link to="/guides" className="transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">PUZZLE&nbsp;</span>GUIDES</Link>
+          <Link to="/guides" className="text-xs sm:text-base transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">PUZZLE&nbsp;</span>GUIDES</Link>
         </div>
         <div>
-          <Link to="/archive" className="transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">HUNT&nbsp;</span>ARCHIVE</Link>
+          <Link to="/archive" className="text-xs sm:text-base transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">HUNT&nbsp;</span>ARCHIVE</Link>
         </div>
         <div>
-          <Link to="/comp" className="transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">WEEKLY&nbsp;</span>COMP</Link>
+          <Link to="/comp" className="text-xs sm:text-base transition duration-300 bg-inherit hover:bg-puzzleBlue-dark p-3"><span className="hidden sm:inline-block">WEEKLY&nbsp;</span>COMP</Link>
         </div>
         <FontAwesomeIcon onClick={toggleDark} className="transition duration-700 bg-inherit hover:bg-puzzleBlue-dark p-3 hover:cursor-pointer" icon={faMoon} size="lg"/>
       </nav>
